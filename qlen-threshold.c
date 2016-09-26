ecb97db77f529f9514466859d5a8db9700f944d1
Danfeng Shan <shandanf@gmail.com>
2016-09-26 19:37:58 +0800

    forget to add the file

---
+++ qlen-threshold.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <sys/types.h>
+#include <string.h>
+#include <sys/queue.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <getopt.h>
+#include <unistd.h>
+
+#include <rte_common.h>
+#include <rte_byteorder.h>
+#include <rte_log.h>
+#include <rte_memory.h>
+#include <rte_memcpy.h>
+#include <rte_memzone.h>
+#include <rte_eal.h>
+#include <rte_per_lcore.h>
+#include <rte_launch.h>
+#include <rte_atomic.h>
+#include <rte_cycles.h>
+#include <rte_prefetch.h>
+#include <rte_lcore.h>
+#include <rte_per_lcore.h>
+#include <rte_branch_prediction.h>
+#include <rte_interrupts.h>
+#include <rte_pci.h>
+#include <rte_random.h>
+#include <rte_debug.h>
+#include <rte_ether.h>
+#include <rte_ethdev.h>
+#include <rte_ring.h>
+#include <rte_mempool.h>
+#include <rte_mbuf.h>
+#include <rte_ip.h>
+#include <rte_tcp.h>
+#include <rte_lpm.h>
+#include <rte_lpm6.h>
+
+#include "main.h"
+
+uint32_t
+qlen_threshold_equal_division(uint32_t port_id) {
+    port_id = port_id << 1; /* prevent warning */
+    uint32_t result = app.buff_size_pkts / app.n_ports;
+#if QUE_IN_BYTES == 1
+    return result * app.mean_pkt_size;
+#else
+    return result;
+#endif
+}
+
+uint32_t
+qlen_threshold_dt(uint32_t port_id) {
+    port_id = port_id << 1; /* prevent warning */
+#if QUE_IN_BYTES == 1
+    return ((app.buff_size_pkts * app.mean_pkt_size - app.buff_occu_bytes) >> app.dt_shift_alpha);
+#else
+    return ((app.buff_size_pkts - app.buff_occu_pkts) >> app.dt_shift_alpha);
+#endif
+}
